14a78fff123d1c807d3b65398bb11ae0b3134418
PikachuHy <pikachuhy@163.com>
2019-04-06 10:38:47 +0800

    build: 完成deb打包

--- src/translations/krabby_en.ts
+++ src/translations/krabby_en.ts
@@ -4,33 +4,33 @@
 <context>
     <name>TypeWidget</name>
     <message>
-        <location filename="../typewidget.cpp" line="195"/>
+        <location filename="../typewidget.cpp" line="198"/>
         <source>Time:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="201"/>
+        <location filename="../typewidget.cpp" line="204"/>
         <source>Speed:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="205"/>
+        <location filename="../typewidget.cpp" line="208"/>
         <source>%1 w/m</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="208"/>
+        <location filename="../typewidget.cpp" line="211"/>
         <source>Progress:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="212"/>
-        <location filename="../typewidget.cpp" line="219"/>
+        <location filename="../typewidget.cpp" line="215"/>
+        <location filename="../typewidget.cpp" line="222"/>
         <source>%1 %</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="215"/>
+        <location filename="../typewidget.cpp" line="218"/>
         <source>Accuracy:</source>
         <translation type="unfinished"></translation>
     </message>
--- src/translations/krabby_zh.ts
+++ src/translations/krabby_zh.ts
@@ -4,33 +4,33 @@
 <context>
     <name>TypeWidget</name>
     <message>
-        <location filename="../typewidget.cpp" line="195"/>
+        <location filename="../typewidget.cpp" line="198"/>
         <source>Time:</source>
         <translation>时间:</translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="201"/>
+        <location filename="../typewidget.cpp" line="204"/>
         <source>Speed:</source>
         <translation>速度:</translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="205"/>
+        <location filename="../typewidget.cpp" line="208"/>
         <source>%1 w/m</source>
         <translation>%1 字符/分</translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="208"/>
+        <location filename="../typewidget.cpp" line="211"/>
         <source>Progress:</source>
         <translation>进度:</translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="212"/>
-        <location filename="../typewidget.cpp" line="219"/>
+        <location filename="../typewidget.cpp" line="215"/>
+        <location filename="../typewidget.cpp" line="222"/>
         <source>%1 %</source>
         <translation>%1 %</translation>
     </message>
     <message>
-        <location filename="../typewidget.cpp" line="215"/>
+        <location filename="../typewidget.cpp" line="218"/>
         <source>Accuracy:</source>
         <translation>正确率:</translation>
     </message>
